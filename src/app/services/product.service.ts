985cc5968c6d12e4a4cab28238e8afb0c428ed17
wcachero <wilson.09c@gmail.com>
2025-02-02 17:06:48 +0800

    sdfs

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { catchError } from 'rxjs/operators';
 export class ProductService {
   //private apiUrl = 'https://wcacheromm.azurewebsites.net/api/LoanProduct/';
 
- private apiUrl= 'http://49.147.72.195/api/customerdetail/api/LoanProduct';
+ private apiUrl= 'http://49.147.72.195/api/LoanProduct';
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<any[]> {
